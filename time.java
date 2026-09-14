35457933829bf3a6bbc31740fc446c2973e6b073
White Tiger (male) <octaviusgao@gmail.com>
2026-09-14 13:34:34 -0700

    Add files via upload

--- time.java
+++ time.java
@@ -5,10 +5,16 @@ public class time {
 		int minute = 16;
 		int second = 41;
 		
+		double current = hour*60*60 + minute*60 + second;
+		double total = 60*60*24;
+		
 		System.out.print(hour*60*60 + minute*60 + second);
 		System.out.println(" seconds passed from midnight");
 		
 		System.out.print(60*60*24 - hour*60*60 + minute*60 + second);
 		System.out.println(" seconds remaining in the day");
+		
+		System.out.print(current/total*100);
+		System.out.println(" percent done with the day");
 	}
 }
