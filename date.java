9f4f2a45500d76a4e530a6621ccf0fff1ec29031
White Tiger (male) <octaviusgao@gmail.com>
2026-09-14 13:15:03 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,28 @@
+
+
+public class date {
+	
+	public static void main (String[] args) {
+		int day = 14;
+		int year = 2026;
+		String week = "Monday";
+		String month = "September";
+		String american = "American format: ";
+		String european = "European format: ";
+		
+		
+		System.out.print(american);
+		System.out.print(week + ", ");
+		System.out.print(month + " ");
+		System.out.print(day + ", ");
+		System.out.println(year);
+		
+		System.out.print(european);
+		System.out.print(week + " ");
+		System.out.print(day + " ");
+		System.out.print(month + " ");
+		System.out.println(year + " ");
+		
+	}
+}
+
